e4aabf5044584e9884f0a4c428e2938bf68a236d
krtkmj <kartik13051@iiitd.ac.in>
2014-11-06 01:20:40 +0530

    Front page done

--- cubane/urls.py
+++ cubane/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'cubane.views.home', name='home'),
+    url(r'^$', 'cubane.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
