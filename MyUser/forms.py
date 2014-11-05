28134432c0c4e80202b52303f7931110a9b198b7
krtkmj <kartik13051@iiitd.ac.in>
2014-11-06 02:46:40 +0530

    signup form created

--- MyUser/forms.py
+++ MyUser/forms.py
@@ -5,3 +5,14 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model=MyUser
         fields=('username','password')
+
+class SignupForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model=MyUser
+        fields=('email','username','firstname','lastname','password','password1',
+              'mobile',
+        )
+        widgets = {
+             'password': forms.PasswordInput(),
+        }
